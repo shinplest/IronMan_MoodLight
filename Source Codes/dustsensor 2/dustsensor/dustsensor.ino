fb7fc51c10fc505169c0573c4d9e0df48d3cacb6
shineceo97 <shineceo97@naver.com>
2019-11-13 13:36:19 +0900

    modify_dust_codes

--- Source Codes/dustsensor 2/dustsensor/dustsensor.ino
+++ Source Codes/dustsensor 2/dustsensor/dustsensor.ino
@@ -3,53 +3,13 @@
 
 #define Voc 0.4
 
-
 int data[10] = {40,40,40,40,40,40,40,40,40,40};
-float avg(int x);
-float DustResult = 0;
+double DustResult = 0;
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(dustLed, OUTPUT);
-}
-
-void loop() {
-  digitalWrite(dustLed, LOW);
-  delayMicroseconds(280);
-  float readDust = analogRead(dustAnalog);
-  delayMicroseconds(40);
-  digitalWrite(dustLed, HIGH);
-  delayMicroseconds(9680); 
-
-  float voltage = readDust * (5.0 / 1024.0);
-  Serial.println(voltage);
-  float dust = (voltage - Voc) / 0.005;
-  DustResult = avg(dust);
-  
-
-  if(DustResult > 100) { 
-     Serial.println("Bad");
-     
-  }
-  else if(DustResult > 50) {
-    Serial.println("Normal");
-  }
-  else {
-    Serial.println("Good");
-  }
-    
-  Serial.print("Dust : ");
-  Serial.print(DustResult);
-  Serial.println("[ug/m3]");
-  delay(1000);
-}
-
-
-
-float avg(int x){
-    unsigned char i;
-    float sum=0;
-    float average=0;
+double avg(int x){
+    int i;
+    double  sum=0;
+    double average=0;
     for(i = 0; i < 10-1; i++){
       data[i]=data[i+1];
     }
@@ -61,3 +21,45 @@ float avg(int x){
     average = sum / 10;
     return average;
   }
+
+double CalculateDust(){
+  digitalWrite(dustLed, LOW);
+  delayMicroseconds(280);
+  double readDust = analogRead(dustAnalog);
+  delayMicroseconds(40);
+  digitalWrite(dustLed, HIGH);
+  delayMicroseconds(9680); 
+
+  double voltage = readDust * (5.0 / 1024.0);
+  Serial.println(voltage);
+  double dust = (voltage - Voc) / 0.005;
+
+  return avg(dust);
+}
+
+void Print2Serial(double DustResult){
+  if(DustResult > 100) { 
+    Serial.println("Bad");
+     
+  }
+  else if(DustResult > 50) {
+    Serial.println("Normal");
+  }
+  else {
+    Serial.println("Good");
+  }
+  Serial.print("Dust : ");
+  Serial.print(DustResult);
+  Serial.println("[ug/m3]");
+  delay(1000);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(dustLed, OUTPUT);
+}
+
+void loop() {
+  DustResult = CalculateDust();
+  Print2Serial(DustResult);
+}
