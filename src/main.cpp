e74cc30c29d22258d387206610b9f52435788158
shineceo97 <shineceo97@naver.com>
2019-11-16 20:29:43 +0900

    add_mise_checked_working

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,44 @@ LiquidCrystal_I2C lcd(0x27, 16, 2); //lcd 객체 선언
 
 
 
+// Choose program options.
+//#define PRINT_RAW_DATA
+#define USE_AVG
+
+// Arduino pin numbers.
+const int sharpLEDPin = 8;   // Arduino digital pin 7 connect to sensor LED.
+const int sharpVoPin = A1;   // Arduino analog pin 5 connect to sensor Vo.
+
+// For averaging last N raw voltage readings.
+#ifdef USE_AVG
+#define N 100
+static unsigned long VoRawTotal = 0;
+static int VoRawCount = 0;
+#endif // USE_AVG
+
+// Set the typical output voltage in Volts when there is zero dust. 
+static float Voc = 0.6;
+
+// Use the typical sensitivity in units of V per 100ug/m3.
+const float K = 0.5;
+  
 /////////////////////////////////////////////////////////////////////////////
 
 // Helper functions to print a data value to the serial monitor.
-void printValue(String text, unsigned int value, bool isLast = false)
-{
+void printValue(String text, unsigned int value, bool isLast = false) {
   Serial.print(text);
   Serial.print("=");
   Serial.print(value);
-  if (!isLast)
-  {
+  if (!isLast) {
     Serial.print(", ");
   }
 }
-void printFValue(String text, float value, String units, bool isLast = false)
-{
+void printFValue(String text, float value, String units, bool isLast = false) {
   Serial.print(text);
   Serial.print("=");
   Serial.print(value);
   Serial.print(units);
-  if (!isLast)
-  {
+  if (!isLast) {
     Serial.print(", ");
   }
 }
@@ -73,6 +90,7 @@ void setup()
   strip.show();
 
   pinMode(APDS9960_INT, INPUT);
+  pinMode(sharpLEDPin, OUTPUT);
 
   attachInterrupt(0, interruptRoutine, FALLING);
   // Initialize APDS-9960 (configure I2C and initial values)
@@ -98,37 +116,87 @@ void setup()
 
 void loop()
 {
- 
-  int Register = analogRead(A0);
-  if( Register>300 && Register<500){
-    lcd.init();
-  }
+ // Turn on the dust sensor LED by setting digital pin LOW.
+  digitalWrite(sharpLEDPin, LOW);
 
-  lcd.setCursor(0, 0); // 1번째, 2라인
-  lcd.print("Gesture Running");
-  lcd.setCursor(0, 1); // 1번째, 2라인
-  lcd.print(turn);
-  turn++;
+  // Wait 0.28ms before taking a reading of the output voltage as per spec.
+  delayMicroseconds(280);
 
-  if (isr_flag == 1)
-  {
-    detachInterrupt(0);
-    handleGesture();
-    isr_flag = 0;
-    attachInterrupt(0, interruptRoutine, FALLING);
-  }
-  getbtstring();
-  bluetoothonoff();
+  // Record the output voltage. This operation takes around 100 microseconds.
+  int VoRaw = analogRead(sharpVoPin);
+  
+  // Turn the dust sensor LED off by setting digital pin HIGH.
+  digitalWrite(sharpLEDPin, HIGH);
 
-  if(VolumeState == 4){
-     //whiteOverRainbow(102, 2);
-     rainbowFade2White(3,3,0);
+  // Wait for remainder of the 10ms cycle = 10000 - 280 - 100 microseconds.
+  delayMicroseconds(9620);
+  
+  // Print raw voltage value (number from 0 to 1023).
+  #ifdef PRINT_RAW_DATA
+  printValue("VoRaw", VoRaw, true);
+  Serial.println("");
+  #endif // PRINT_RAW_DATA
+  
+  // Use averaging if needed.
+  float Vo = VoRaw;
+  #ifdef USE_AVG
+  VoRawTotal += VoRaw;
+  VoRawCount++;
+  if ( VoRawCount >= N ) {
+    Vo = 1.0 * VoRawTotal / N;
+    VoRawCount = 0;
+    VoRawTotal = 0;
+  } else {
+    return;
   }
+  #endif // USE_AVG
 
-  if(LightState == false){
-    pulseWhite(5);
-    //whiteOverRainbow(100, 2);
+  // Compute the output voltage in Volts.
+  Vo = Vo / 1024.0 * 5.0;
+  printFValue("Vo", Vo*1000.0, "mV");
+
+  // Convert to Dust Density in units of ug/m3.
+  float dV = Vo - Voc;
+  if ( dV < 0 ) {
+    dV = 0;
+    Voc = Vo;
   }
+  float dustDensity = dV / K * 100.0;
+  printFValue("DustDensity", dustDensity, "ug/m3", true);
+  Serial.println("");
+
+
+
+  // int Register = analogRead(A0);
+  // if( Register>300 && Register<500){
+  //   lcd.init();
+  // }
+
+  // lcd.setCursor(0, 0); // 1번째, 2라인
+  // lcd.print("Gesture Running");
+  // lcd.setCursor(0, 1); // 1번째, 2라인
+  // lcd.print(turn);
+  // turn++;
+
+  // if (isr_flag == 1)
+  // {
+  //   detachInterrupt(0);
+  //   handleGesture();
+  //   isr_flag = 0;
+  //   attachInterrupt(0, interruptRoutine, FALLING);
+  // }
+  // getbtstring();
+  // bluetoothonoff();
+
+  // if(VolumeState == 4){
+  //    //whiteOverRainbow(102, 2);
+  //    rainbowFade2White(3,3,0);
+  // }
+
+  // if(LightState == false){
+  //   pulseWhite(5);
+  //   //whiteOverRainbow(100, 2);
+  // }
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
