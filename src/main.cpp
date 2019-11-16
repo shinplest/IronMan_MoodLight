11b152f675cb104344d925db4d0305bf67e5e258
shineceo97 <shineceo97@naver.com>
2019-11-16 20:35:08 +0900

    여전히동작

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,14 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2); //lcd 객체 선언
 
 
-
-// Choose program options.
-//#define PRINT_RAW_DATA
-#define USE_AVG
-
 // Arduino pin numbers.
 const int sharpLEDPin = 8;   // Arduino digital pin 7 connect to sensor LED.
 const int sharpVoPin = A1;   // Arduino analog pin 5 connect to sensor Vo.
 
-// For averaging last N raw voltage readings.
-#ifdef USE_AVG
 #define N 100
 static unsigned long VoRawTotal = 0;
 static int VoRawCount = 0;
-#endif // USE_AVG
+
 
 // Set the typical output voltage in Volts when there is zero dust. 
 static float Voc = 0.6;
@@ -116,30 +109,13 @@ void setup()
 
 void loop()
 {
- // Turn on the dust sensor LED by setting digital pin LOW.
   digitalWrite(sharpLEDPin, LOW);
-
-  // Wait 0.28ms before taking a reading of the output voltage as per spec.
   delayMicroseconds(280);
-
-  // Record the output voltage. This operation takes around 100 microseconds.
   int VoRaw = analogRead(sharpVoPin);
-  
-  // Turn the dust sensor LED off by setting digital pin HIGH.
   digitalWrite(sharpLEDPin, HIGH);
-
-  // Wait for remainder of the 10ms cycle = 10000 - 280 - 100 microseconds.
   delayMicroseconds(9620);
   
-  // Print raw voltage value (number from 0 to 1023).
-  #ifdef PRINT_RAW_DATA
-  printValue("VoRaw", VoRaw, true);
-  Serial.println("");
-  #endif // PRINT_RAW_DATA
-  
-  // Use averaging if needed.
   float Vo = VoRaw;
-  #ifdef USE_AVG
   VoRawTotal += VoRaw;
   VoRawCount++;
   if ( VoRawCount >= N ) {
@@ -149,7 +125,6 @@ void loop()
   } else {
     return;
   }
-  #endif // USE_AVG
 
   // Compute the output voltage in Volts.
   Vo = Vo / 1024.0 * 5.0;
